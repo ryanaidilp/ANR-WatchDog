1273fb72765e189d2fc3387e3cdb51e452978955
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-12-07 02:59:55 +0700

    fix: make signing conditional for JitPack compatibility
    - Skip signing when signing.keyId property is not available
    - Allows JitPack builds to succeed without GPG keys
    - Bump version to 2.0.1

--- anr-watchdog/build.gradle.kts
+++ anr-watchdog/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.anrwatchdog"
-version = "2.0.0"
+version = "2.0.1"
 
 android {
     namespace = "com.github.anrwatchdog"
@@ -125,6 +125,16 @@ publishing {
     }
 }
 
+// Only sign when signing properties are available (not on JitPack)
+val hasSigningKey = findProperty("signing.keyId") != null
+
 signing {
-    sign(publishing.publications["release"])
+    setRequired { false }
+    if (hasSigningKey) {
+        sign(publishing.publications["release"])
+    }
+}
+
+tasks.withType<Sign>().configureEach {
+    onlyIf { hasSigningKey }
 }

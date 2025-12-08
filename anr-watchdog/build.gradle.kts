47434eb2b3d4a8e2cf9ccb7a42197210d603987a
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2025-12-08 11:53:15 +0800

    Update version from 2.1.0 to 2.2.0-SNAPSHOT

--- anr-watchdog/build.gradle.kts
+++ anr-watchdog/build.gradle.kts
@@ -6,11 +6,7 @@ plugins {
 }
 
 group = "com.github.anrwatchdog"
-<<<<<<< HEAD
 version = "2.2.0-SNAPSHOT"
-=======
-version = "2.1.0"
->>>>>>> master
 
 android {
     namespace = "com.github.anrwatchdog"
